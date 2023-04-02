55eaa02120fe65b7c7c11e874bb951bb1d94fe6c
Bruna Fonseca <bruna.mfonseca@mercadolivre.com>
2023-04-02 13:20:17 -0300

    Initial Commit

---
+++ weather-forecast/weather_forecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_forecastApp.swift
+//  weather-forecast
+//
+//  Created by Bruna Marques De Oliveira Fonseca on 02/04/23.
+//
+
+import SwiftUI
+
+@main
+struct weather_forecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
